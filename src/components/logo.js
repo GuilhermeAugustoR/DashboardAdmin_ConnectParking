441142fd961aaec83113bff471492968e55bf689
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2022-07-02 17:15:44 -0300

    first commit

---
+++ src/components/logo.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+
+export const Logo = styled((props) => {
+  const { variant, ...other } = props;
+
+  const color = variant === "light" ? "#C1C4D6" : "#5048E5";
+
+  return (
+    <div {...other}>
+      <h3 style={{}}>ConnectParking</h3>
+    </div>
+  );
+})``;
+
+Logo.defaultProps = {
+  variant: "primary",
+};
+
+Logo.propTypes = {
+  variant: PropTypes.oneOf(["light", "primary"]),
+};
---
+++ src/pages/index.js
@@ -0,0 +1,110 @@
+import Head from "next/head";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Box, Button, Container, Grid, Link, TextField, Typography } from "@mui/material";
+
+const Login = () => {
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      email: "demo@devias.io",
+      password: "Password123",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Must be a valid email").max(255).required("Email is required"),
+      password: Yup.string().max(255).required("Password is required"),
+    }),
+    onSubmit: () => {
+      router.push("/");
+    },
+  });
+
+  return (
+    <>
+      <Head>
+        <title>Login | Material Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: "center",
+          display: "flex",
+          flexGrow: 1,
+          minHeight: "100%",
+        }}
+      >
+        <Container maxWidth="sm">
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography color="textPrimary" variant="h4">
+                Login
+              </Typography>
+              <Typography color="textSecondary" gutterBottom variant="body2">
+                Faça login na plataforma interna
+              </Typography>
+            </Box>
+
+            <TextField
+              error={Boolean(formik.touched.email && formik.errors.email)}
+              fullWidth
+              helperText={formik.touched.email && formik.errors.email}
+              label="Email Address"
+              margin="normal"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="Password"
+              margin="normal"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+              variant="outlined"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                disabled={formik.isSubmitting}
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+                href="/dashboard"
+              >
+                Entrar
+              </Button>
+            </Box>
+            <Typography color="textSecondary" variant="body2">
+              Não tem conta?{" "}
+              <NextLink href="/register">
+                <Link
+                  to="/register"
+                  variant="subtitle2"
+                  underline="hover"
+                  sx={{
+                    cursor: "pointer",
+                  }}
+                >
+                  Cadastrar
+                </Link>
+              </NextLink>
+            </Typography>
+          </form>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
